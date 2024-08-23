a47184f88f369162921b947e6616c14701226a81
Ian Jarvis <ijarvis12@gmail.com>
2024-08-23 09:19:20 -0500

    Update vi.sh

--- vi.sh
+++ vi.sh
@@ -6,19 +6,17 @@ IFS='\n\t '
 
 GFILE="$1"
 
-bind -m 'vi-insert' '"\e[A":previous-screen-line'
-bind -m 'vi-insert' '"\e[B":next-screen-line'
-bind -m 'vi-move' '"k":previous-screen-line'
-bind -m 'vi-move' '"j":next-screen-line'
-bind -m 'vi-insert' '"\r":self-insert'
-bind -m 'vi-insert' '"\t":self-insert'
+bind -m vi-insert '"\e[A":previous-screen-line'
+bind -m vi-insert '"\e[B":next-screen-line'
+bind -m vi-move '"k":previous-screen-line'
+bind -m vi-move '"j":next-screen-line'
+bind -m vi-insert '"\r":self-insert'
+bind -m vi-insert '"\t":self-insert'
 
-read -er -i "$(cat $GFILE)" gettext
+INS_TEXT=$(if [[ -n "$GFILE" ]]; then cat "$GFILE"; else echo; fi)
 
-if [[ -z "$GFILE" ]]; then
-  read -er -p "Save as: " GFILE
-fi
+read -er -i "$INS_TEXT" gettext
 
-if [[ -n "$GFILE" ]]; then
-  echo -e "$gettext" > "$GFILE"
-fi
+if [[ -z "$GFILE" ]]; then read -er -p "Save as: " GFILE; fi
+
+if [[ -n "$GFILE" ]]; then echo -e "$gettext" > "$GFILE"; fi
