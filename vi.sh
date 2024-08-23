d1b0dd4bcd66828f0c2e73b26182e8656f2d252a
Ian Jarvis <ijarvis12@gmail.com>
2024-08-23 11:49:43 -0500

    Update vi.sh

--- vi.sh
+++ vi.sh
@@ -14,7 +14,7 @@ bind -m vi-insert '"\t":self-insert'
 
 INS_TEXT="$(if [[ -n $GFILE && -e $GFILE ]]; then cat $GFILE; else echo; fi)"
 
-read -er -d $'^D' -i "$INS_TEXT" gettext
+read -er -d $'\04' -i "$INS_TEXT" gettext
 
 if [[ -z "$GFILE" ]]; then read -er -p "Save as: " GFILE; fi
 
