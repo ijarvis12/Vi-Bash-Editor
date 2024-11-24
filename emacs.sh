cdb5c2034ba917e2557f364f88d225fbbc1af615
Ian Jarvis <ijarvis12@gmail.com>
2024-11-24 11:11:15 -0600

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -8,10 +8,12 @@ bind "set blink-matching-paren ON"
 bind "set horizontal-scroll-mode ON"
 bind -m emacs '"\e[A":previous-screen-line'
 bind -m emacs '"\e[B":next-screen-line'
+bind -m emacs '"\C-p":previous-screen-line'
+bind -m emacs '"\C-n":next-screen-line'
 bind -m emacs '"\n":self-insert'
 bind -m emacs '"\r":"\n"'
 bind -m emacs '"\t":tab-insert'
-bind -m emacs '"\C-x\C-s":accept-line'
+bind -m emacs-ctlx '"s":accept-line'
 
 INSERT_TEXT="$(if [[ -n $GFILE && -e $GFILE ]]; then cat $GFILE; else echo; fi)"
 
@@ -20,6 +22,7 @@ IFS=
 read -er -i "$INSERT_TEXT"
 
 bind -m emacs '"\n":accept-line'
+bind -m emacs '"\r":accept-line'
 
 if [[ -z "$GFILE" ]]; then read -er -p "Save as: " GFILE; fi
 
