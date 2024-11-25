cd5bb9fc80a5995ef1529a8ff371f03523260dd8
Ian Jarvis <ijarvis12@gmail.com>
2024-11-25 12:58:47 -0600

    Update vi.sh

--- vi.sh
+++ vi.sh
@@ -31,7 +31,7 @@ bind -m vi-insert '"\n":accept-line'
 bind -m vi-insert '"\r":accept-line'
 
 if [[ -z "$GFILE" ]]; then
-  # place curosr at bottom
+  # place cursor at bottom
   printf "\e[$LINES;0H"
   read -er -p "Save as: " GFILE
 fi
