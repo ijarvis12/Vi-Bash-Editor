670e97029b5dacb6c120bb472063e6f2c7409787
Ian Jarvis <ijarvis12@gmail.com>
2024-11-24 21:44:09 -0600

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -5,7 +5,7 @@ set -o emacs
 GFILE="$@"
 
 bind "set blink-matching-paren ON"
-bind "set horizontal-scroll-mode ON"
+bind "set horizontal-scroll-mode OFF"
 bind -m emacs '"\e[A":previous-screen-line'
 bind -m emacs '"\e[B":next-screen-line'
 bind -m emacs '"\C-p":previous-screen-line'
@@ -15,7 +15,10 @@ bind -m emacs '"\r":"\n"'
 bind -m emacs '"\t":tab-insert'
 bind -m emacs-ctlx '"s":accept-line'
 
-INSERT_TEXT="$(if [[ -n $GFILE && -e $GFILE ]]; then cat $GFILE; else echo; fi)"
+# enable alt buffer screen and place cursor at top
+printf "\e[?1049h\e[H"
+
+INSERT_TEXT="$(if [[ -n $GFILE && -e $GFILE ]]; then cat $GFILE; fi)"
 
 IFS=
 
@@ -24,6 +27,13 @@ read -er -i "$INSERT_TEXT"
 bind -m emacs '"\n":accept-line'
 bind -m emacs '"\r":accept-line'
 
-if [[ -z "$GFILE" ]]; then read -er -p "Save as: " GFILE; fi
+if [[ -z "$GFILE" ]]; then
+  # place curosr at bottom
+  printf "\e[$LINES;0H"
+  read -er -p "Save as: " GFILE
+fi
 
 if [[ -n "$GFILE" ]]; then printf "$REPLY" > "$GFILE"; fi
+
+# disable alt buffer screen
+printf "\e[?1049l"
