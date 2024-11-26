89fe6b2483d18d64752f2022d34c3eae194d8770
Ian Jarvis <ijarvis12@gmail.com>
2024-11-26 08:43:44 -0600

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -10,7 +10,16 @@ bind -m emacs '"\e[A":previous-screen-line'
 bind -m emacs '"\e[B":next-screen-line'
 bind -m emacs '"\C-p":previous-screen-line'
 bind -m emacs '"\C-n":next-screen-line'
-bind -m emacs '"\n":self-insert'
+
+GETTEXT=
+
+newline-insert() {
+  READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}"$'\n'"${READLINE_LINE:$READLINE_POINT:${#READLINE_LINE} - $READLINE_POINT}"
+  READLINE_POINT+=1
+  GETTEXT="$READLINE_LINE"
+}
+
+bind -m emacs -x '"\n":newline-insert'
 bind -m emacs '"\r":"\n"'
 bind -m emacs '"\t":tab-insert'
 bind -m emacs-ctlx '"s":accept-line'
@@ -19,10 +28,9 @@ INSERT_TEXT="$(if [[ -n $GFILE && -e $GFILE ]]; then cat $GFILE; fi)"
 
 IFS=
 
-read -er -i "$INSERT_TEXT" GETTEXT
+read -er -i "$INSERT_TEXT"
 
 bind -m emacs '"\n":accept-line'
-bind -m emacs '"\r":accept-line'
 
 if [[ -z "$GFILE" ]]; then read -er -p "Save as: " GFILE; fi
 
