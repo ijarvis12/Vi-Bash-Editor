e53ab0216ed0042c7faed647f87ce3b4d566009f
Ian Jarvis <ijarvis12@gmail.com>
2024-12-02 14:31:30 -0600

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -10,16 +10,7 @@ bind -m emacs '"\e[A":previous-screen-line'
 bind -m emacs '"\e[B":next-screen-line'
 bind -m emacs '"\C-p":previous-screen-line'
 bind -m emacs '"\C-n":next-screen-line'
-
-GETTEXT=
-
-newline-insert() {
-  READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}"$'\n'"${READLINE_LINE:$READLINE_POINT:${#READLINE_LINE} - $READLINE_POINT}"
-  READLINE_POINT+=1
-  GETTEXT="$READLINE_LINE"
-}
-
-bind -m emacs -x '"\n":newline-insert'
+bind -m emacs '"\n":self-insert'
 bind -m emacs '"\r":"\n"'
 bind -m emacs '"\t":tab-insert'
 bind -m emacs-ctlx '"s":accept-line'
@@ -28,10 +19,11 @@ if [[ -n "$GFILE" && -e "$GFILE" ]]; then INSERT_TEXT=$(<"$GFILE"); else INSERT_
 
 IFS=
 
-read -er -i "$INSERT_TEXT"
+read -er -d$'\04' -i "$INSERT_TEXT" GETTEXT
 
 bind -m emacs '"\n":accept-line'
+bind -m emacs '"\r":accept-line'
 
 if [[ -z "$GFILE" ]]; then read -er -p "Save as: " GFILE; fi
 
-if [[ -n "$GFILE" ]]; then printf "$GETTEXT" > "$GFILE"; fi
+if [[ -n "$GFILE" ]]; then echo "$GETTEXT" > "$GFILE"; fi
